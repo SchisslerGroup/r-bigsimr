942d79b2d2593b01f48a50fe3ad67f678780dc4a
Alex Knudson <aknudson@nevada.unr.edu>
2020-06-07 15:33:42 -0700

    Added/exported rmvn() function for fast random mv normal generation

--- R/rmvn.R
+++ R/rmvn.R
@@ -2,11 +2,7 @@
 
   # Any RNG in jax requires a key. If none is supplied by the user, then it
   # should be generated at random.
-  if (is.null(seed)) {
-    set.seed(seed)
-    seed <- as.integer(sample(1:1000, 1))
-  }
-  key = jax$random$PRNGKey(as.integer(seed))
+  key = jax$random$PRNGKey(sample(.Random.seed, 1))
 
   d <- ncol(rho)
 
@@ -15,6 +11,8 @@
   mu_np    = reticulate::np_array(mu)
 
   rho_chol = numpy$linalg$cholesky(rho_np)
+
+  mvnfast::rmvn()
 
   z      = numpy$random$normal(0.0, 1.0, size)
 
@@ -27,3 +25,35 @@
 
   reticulate::py_to_r(z)
 }
+
+
+#' Simulate random data from a multivariate normal distribution
+#'
+#' This function is designed to be 'bare bones' and does not check that the
+#'   given covariance matrix is positive semi-definite.
+#'
+#' @param n number of random vectors to be simulated.
+#' @param mu vector of length d, representing the mean.
+#' @param sigma covariance matrix (d x d). Alternatively is can be the cholesky
+#'   decomposition of the covariance. In that case isChol should be set to TRUE.
+#' @param isChol boolean set to true if sigma is the cholesky decomposition of
+#'   the covariance matrix.
+#' @export
+rmvn <- function(n, mu, sigma, isChol = FALSE) {
+
+  S    = jax$device_put(reticulate::np_array(sigma)) #
+  m    = jax$device_put(reticulate::np_array(mu))
+  key  = jax$random$PRNGKey(sample(.Random.seed, 1))
+
+  if (isChol) {
+    size = reticulate::tuple(as.integer(n), as.integer(ncol(sigma)))
+    z    = jax$numpy$add(jax$numpy$matmul(jax$random$normal(key, size), S), m)
+  } else {
+    size = reticulate::tuple(as.integer(n))
+    z    = jax$random$multivariate_normal(key, m, S, size)
+  }
+
+  reticulate::py_to_r(jax$device_get(z))
+
+}
+
