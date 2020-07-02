e693b332adc0d3bb63e3f444e96854d5e92f968e
Alex Knudson <aknudson@nevada.unr.edu>
2020-07-02 11:08:22 -0700

    created rho_bounds_cc as independent function

--- test/prototype_Xioa_Zhou/pearson_matching.jl
+++ test/prototype_Xioa_Zhou/pearson_matching.jl
@@ -2,7 +2,7 @@ import Statistics: mean, std, quantile
 import FastGaussQuadrature: gausshermite
 import Polynomials: Polynomial
 import IntervalRootFinding: roots
-import Memoize: @memoize
+import Memoize: @memoize, get_cache, clear_cache
 
 using Distributions
 using IntervalArithmetic
@@ -15,6 +15,9 @@ margins = [
 ]
 
 
+"""
+The Probabilists' version of the Hermite polynomials
+"""
 @memoize function H(x, n::Int)
     if n == 0
         return ones(size(x))
@@ -25,11 +28,14 @@ margins = [
     end
 end
 
-@time H(-10:0.1:10, 250)
 
+"""
+Convert a standard normal variable to any given marginal distribution
+"""
 function z2x(margin, x)
     quantile.(margin, cdf.(Normal(0,1), x))
 end
+
 
 
 function get_coefs(margin, n)
@@ -43,10 +49,6 @@ function get_coefs(margin, n)
     end
     c
 end
-
-get_coefs(margins[1], 11)
-get_coefs(margins[2], 11)
-get_coefs(margins[3], 11)
 
 
 """
@@ -69,8 +71,8 @@ function rho_z_cc(ρx, marginᵢ, marginⱼ; n::Int=3)
     c1 = -μᵢ*μⱼ
     c2 =  1 / (σᵢ*σⱼ)
     kab = factorial.(k).*a.*b
-    ρx_l = c1*c2 + c2*sum((-1).^k .* kab)
-    ρx_u = c1*c2 + c2*sum(kab)
+    ρx_l = c2*(c1 + sum((-1).^k .* kab))
+    ρx_u = c2*(c1 + sum(kab))
     return (ρx_l, ρx_u)
 
     coef = [c2*a[k+1]*b[k+1]*factorial(k) for k = 1:n]
@@ -80,11 +82,31 @@ function rho_z_cc(ρx, marginᵢ, marginⱼ; n::Int=3)
     return (x -> Polynomial(coef)(x), (ρx_l, ρx_u), c1, c2)
 end
 
-rho_z_cc(0, margins[1], margins[2])
-rho_z_cc(0, margins[1], margins[3])
-rho_z_cc(0, margins[2], margins[3])
-rho_z_cc(0, margins[1], margins[1])
-rho_z_cc(0, margins[2], margins[2])
-rho_z_cc(0, margins[3], margins[3])
 
-roots(Polynomial(c))
+function rho_z_bounds_cc(marginᵢ, marginⱼ; n::Int=3)
+    μᵢ = mean(marginᵢ)
+    σᵢ = std(marginᵢ)
+    μⱼ = mean(marginⱼ)
+    σⱼ = std(marginⱼ)
+
+    # Eq (25)
+    k = 0:1:n
+    a = get_coefs(marginᵢ, n)
+    b = get_coefs(marginⱼ, n)
+    # return (μᵢ, σᵢ, μⱼ, σⱼ, a, b)
+
+    # Eq (22)
+    c1 = -μᵢ*μⱼ
+    c2 =  1 / (σᵢ*σⱼ)
+    kab = factorial.(k).*a.*b
+    ρx_l = c1*c2 + c2*sum((-1).^k .* kab)
+    ρx_u = c1*c2 + c2*sum(kab)
+    (ρx_l, ρx_u)
+end
+
+rho_z_bounds(margins[1], margins[1])
+rho_z_bounds(margins[1], margins[2])
+rho_z_bounds(margins[1], margins[3])
+rho_z_bounds(margins[2], margins[2])
+rho_z_bounds(margins[2], margins[3])
+rho_z_bounds(margins[3], margins[3])
