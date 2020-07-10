6777b1f4e4a0d0a42db620708d82ee52e0dfa8f5
Alex Knudson <aknudson@nevada.unr.edu>
2020-07-10 11:14:37 -0700

    pearson matching cc return the midpoint of the root interval

--- scratch/pearson_matching_Xioa_Zhou/pc_cont_cont.jl
+++ scratch/pearson_matching_Xioa_Zhou/pc_cont_cont.jl
@@ -41,16 +41,29 @@ function rho_z_cc(ρx, marginᵢ, marginⱼ; n::Int=3)
     c1 = -μᵢ*μⱼ
     c2 =  1 / (σᵢ*σⱼ)
     kab = factorial.(k).*a.*b
+    ρx_l = c1*c2 + c2*sum((-1).^k .* kab)
+    ρx_u = c1*c2 + c2*sum(kab)
 
+    # Coefficients for Eq (19)
     coef = c2 .* [a[k+1] * b[k+1] * factorial(k) for k = 1:n]
     coef = [c1*c2 + c2*a[1]*b[1] - ρx; coef]
-    # return coef
 
-    return P(x) = Polynomial(coef)(x)
+    # Polynomial from Eq (19) and its derivative
+    P(x)  = Polynomial(coef)(x)
+    dP(x) = Polynomial((1:n) .* coef[2:end])(x)
+
+    # Solve the polynomial over the interval [-1, 1]
+    r = roots(P, dP, -1..1)
+
+    # If no root is found in the interval [-1, 1], then return the upper or
+    # lower bound depending on the sign of ρx. E.g. if ρx = -0.8, then return
+    # ρx_l as the root
+    if length(r) == 0
+        return ρx < 0 ? ρx_l : ρx_u
+    else
+        return mid(r[1].interval)
+    end
 end
-
-Q = rho_z_cc(-0.9, margins[1], margins[1])
-roots(Q, -1..1)
 
 
 function rho_z_bounds_cc(marginᵢ, marginⱼ; n::Int=3)
@@ -73,6 +86,28 @@ function rho_z_bounds_cc(marginᵢ, marginⱼ; n::Int=3)
 
     (ρx_l, ρx_u)
 end
+
+
+@time P  = rho_z_cc(-1.0, margins[1], margins[1])
+@time P  = rho_z_cc(-1.0, margins[1], margins[2])
+@time P  = rho_z_cc(-1.0, margins[1], margins[3])
+@time P  = rho_z_cc(-1.0, margins[2], margins[2])
+@time P  = rho_z_cc(-1.0, margins[2], margins[3])
+@time P  = rho_z_cc(-1.0, margins[3], margins[3])
+
+@time P  = rho_z_cc(0.0, margins[1], margins[1])
+@time P  = rho_z_cc(0.0, margins[1], margins[2])
+@time P  = rho_z_cc(0.0, margins[1], margins[3])
+@time P  = rho_z_cc(0.0, margins[2], margins[2])
+@time P  = rho_z_cc(0.0, margins[2], margins[3])
+@time P  = rho_z_cc(0.0, margins[3], margins[3])
+
+@time P  = rho_z_cc(1.0, margins[1], margins[1])
+@time P  = rho_z_cc(1.0, margins[1], margins[2])
+@time P  = rho_z_cc(1.0, margins[1], margins[3])
+@time P  = rho_z_cc(1.0, margins[2], margins[2])
+@time P  = rho_z_cc(1.0, margins[2], margins[3])
+@time P  = rho_z_cc(1.0, margins[3], margins[3])
 
 rho_z_bounds_cc(margins[1], margins[1])
 rho_z_bounds_cc(margins[1], margins[2])
