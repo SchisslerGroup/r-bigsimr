48bc0ec18db1c4e0b7131155f68a12e7ac06f9e7
Alex Knudson <aknudson@nevada.unr.edu>
2021-02-09 12:18:46 -0800

    basic implementation working
    All functions work, though the implementation is clunky for now.

--- R/bigsimr.R
+++ R/bigsimr.R
@@ -8,19 +8,20 @@
 #' @param pkg_check logical, check for MvSim.jl package and install if necessary
 #' @param ... Parameters are passed down to JuliaCall::julia_setup
 #'
-#' @examples
-#'
-#' \donttest{ ## bigsimr_setup() is time-consuming and requires Julia+MvSim.jl
-#'
-#' bigsimr::bigsimr_setup()
-#'
-#' }
-#'
 #' @export
-bigsimr_setup <- function (pkg_check=TRUE,...){
-  julia <- JuliaCall::julia_setup(installJulia=TRUE,...)
-  if(pkg_check) {
-    JuliaCall::julia_install_package_if_needed("https://github.com/adknudson/MvSim.jl")
-  }
+bigsimr_setup <- function (pkg_check = TRUE, ...){
+  julia <- JuliaCall::julia_setup(installJulia = TRUE, ...)
+  JuliaCall::julia_install_package_if_needed("https://github.com/adknudson/MvSim.jl")
   JuliaCall::julia_library("MvSim")
+  functions <- JuliaCall::julia_eval("filter(isascii, replace.(string.(propertynames(MvSim)),\"!\"=>\"_bang\"))")
+  functions <- c(functions, "rand")
+  MvSim <- JuliaCall::julia_pkg_import("MvSim", functions)
+
+  MvSim$Pearson  <- JuliaCall::julia_eval("Pearson")
+  MvSim$Spearman <- JuliaCall::julia_eval("Spearman")
+  MvSim$Kendall  <- JuliaCall::julia_eval("Kendall")
+
+  JuliaCall::autowrap("MvDistribution")
+
+  MvSim
 }
---
+++ R/datasets.R
@@ -0,0 +1,4 @@
+#' BRCA dataset
+#'
+#' 200 of the highest expressing genes from the BRCA data set.
+"brca"
