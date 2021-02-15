050450454f17271f3a1d00bb96cec2ddc48cafa8
Alex Knudson <aknudson@nevada.unr.edu>
2021-02-15 09:22:02 -0800

    Bigsimr.jl is now a registered package

--- R/bigsimr.R
+++ R/bigsimr.R
@@ -13,8 +13,7 @@ bigsimr_setup <- function (pkg_check = TRUE, ...){
   julia <- JuliaCall::julia_setup(installJulia = TRUE, ...)
 
   if (pkg_check) {
-    JuliaCall::julia_install_package_if_needed("https://github.com/adknudson/Bigsimr.jl")
-    # JuliaCall::julia_install_package_if_needed("Bigsimr")
+    JuliaCall::julia_install_package_if_needed("Bigsimr")
   }
 
   JuliaCall::julia_library("Bigsimr")
