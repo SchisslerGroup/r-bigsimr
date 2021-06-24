49e4b6babe78b7fb61542cdfaac0e63d318a9f31
Duc Tran <duct@nevada.unr.edu>
2021-06-24 10:34:27 -0700

    pass CRAN submission

--- R/distributions.R
+++ R/distributions.R
@@ -3,6 +3,23 @@
 #' This function initializes the Distributions package that many of the bigsimr
 #' functions work with.
 #'
+#' @return Return the imported wrapper of Distributions.jl Julia package
+#' @examples
+#' \donttest{
+#' library(bigsimr)
+#' bs   <- bigsimr::bigsimr_setup()
+#' dist <- bigsimr::distributions_setup()
+#'
+#' JuliaCall::julia_eval('using Random; Random.seed!(1);')
+#' # Generate random target correlation matrix
+#' target_corr <- bs$cor_randPD(3)
+#' # Set the margins of variables
+#' margins <- c(dist$Binomial(20, 0.2), dist$Beta(2, 3), dist$LogNormal(3, 1))
+#' # Adjust target correlation matrix using Pearson matching
+#' adjusted_corr <- bs$pearson_match(target_corr, margins)
+#' # Generate random vectors
+#' x <- bs$rvec(10000, adjusted_corr, margins)
+#' }
 #' @export
 distributions_setup <- function() {
   JuliaCall::julia_install_package_if_needed("Distributions")
