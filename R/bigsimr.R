060fdba18847e96dc43c0b981b1c243ad1bb355e
Duc Tran <duct@nevada.unr.edu>
2021-07-27 10:16:28 -0700

    replace donttest

--- R/bigsimr.R
+++ R/bigsimr.R
@@ -10,20 +10,21 @@
 #'
 #' @return Return the imported wrapper of Bigsimr.jl Julia package
 #' @examples
-#' \donttest{
-#' library(bigsimr)
-#' bs   <- bigsimr::bigsimr_setup()
-#' dist <- bigsimr::distributions_setup()
+#' ## bigsimr_setup() is time-consuming and requires Julia+Bigsimr.jl
+#' if (Sys.which('julia')!=''){
+#'   library(bigsimr)
+#'   bs   <- bigsimr::bigsimr_setup()
+#'   dist <- bigsimr::distributions_setup()
 #'
-#' JuliaCall::julia_eval('using Random; Random.seed!(1);')
-#' # Generate random target correlation matrix
-#' target_corr <- bs$cor_randPD(3)
-#' # Set the margins of variables
-#' margins <- c(dist$Binomial(20, 0.2), dist$Beta(2, 3), dist$LogNormal(3, 1))
-#' # Adjust target correlation matrix using Pearson matching
-#' adjusted_corr <- bs$pearson_match(target_corr, margins)
-#' # Generate random vectors
-#' x <- bs$rvec(10000, adjusted_corr, margins)
+#'   JuliaCall::julia_eval('using Random; Random.seed!(1);')
+#'   # Generate random target correlation matrix
+#'   target_corr <- bs$cor_randPD(3)
+#'   # Set the margins of variables
+#'   margins <- c(dist$Binomial(20, 0.2), dist$Beta(2, 3), dist$LogNormal(3, 1))
+#'   # Adjust target correlation matrix using Pearson matching
+#'   adjusted_corr <- bs$pearson_match(target_corr, margins)
+#'   # Generate random vectors
+#'   x <- bs$rvec(10000, adjusted_corr, margins)
 #' }
 #' @export
 bigsimr_setup <- function (pkg_check = TRUE, ...){
