48755bef73908b78db6465db8fadf91f5c0d9b7f
aknudson <alexk.706@gmail.com>
2024-02-20 21:59:55 -0800

    updated docs and tests

--- R/distributions.R
+++ R/distributions.R
@@ -4,23 +4,6 @@
 #' functions work with.
 #'
 #' @return Return the imported wrapper of Distributions.jl Julia package
-#' @examples
-#' ## distributions_setup() is time-consuming and requires Julia+Distributions.jl
-#' \dontrun{
-#'   library(bigsimr)
-#'   bs   <- bigsimr::bigsimr_setup()
-#'   dist <- bigsimr::distributions_setup()
-#'
-#'   JuliaCall::julia_eval('using Random; Random.seed!(1);')
-#'   # Generate random target correlation matrix
-#'   target_corr <- bs$cor_randPD(3)
-#'   # Set the margins of variables
-#'   margins <- c(dist$Binomial(20, 0.2), dist$Beta(2, 3), dist$LogNormal(3, 1))
-#'   # Adjust target correlation matrix using Pearson matching
-#'   adjusted_corr <- bs$pearson_match(target_corr, margins)
-#'   # Generate random vectors
-#'   x <- bs$rvec(10000, adjusted_corr, margins)
-#' }
 #' @export
 distributions_setup <- function() {
   JuliaCall::julia_install_package_if_needed("Distributions")
